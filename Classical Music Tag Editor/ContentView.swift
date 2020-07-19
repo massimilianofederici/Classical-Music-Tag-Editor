a63aadc260b366f534ec0b838d4bd92e34a78fd2
Massimiliano Federici <realmassy76@gmail.com>
2020-07-19 20:56:31 +0100

    Initial Commit

---
+++ Classical Music Tag Editor/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Classical Music Tag Editor
+//
+//  Created by Massimiliano Federici on 19/07/2020.
+//  Copyright © 2020 Massimiliano Federici. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
